5a83cae1aa1ddedafda560c276270c1321ed0ee0
tiara2187 <tiaramuttianingtyas23@gmail.com>
2021-04-14 14:01:19 +0700

    create generate component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular';
+  number = 0;
+  increaseNumber():any{
+    this.number++;
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ServeComponent } from './serve/serve.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { AlertSuccessComponent } from './alert-success/alert-success.component';
+import { AlertWarningComponent } from './alert-warning/alert-warning.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServeComponent,
+    HeaderComponent,
+    FooterComponent,
+    AlertSuccessComponent,
+    AlertWarningComponent
   ],
   imports: [
     BrowserModule,
